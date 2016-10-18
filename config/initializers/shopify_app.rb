fa22964364f7bd9d8d00662cfe2e20fd1f84441e
voodoobettie <ingrid@frankenmedia.ca>
2016-10-17 22:00:00 -0700

    changed redirect uri

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,5 +2,6 @@ ShopifyApp.configure do |config|
   config.api_key = "1234xyz"
   config.secret = "<secret>"
   config.scope = "read_orders, read_products"
+  config.redirect_uri = "http://localhost:3000/auth/shopify/callback"
   config.embedded_app = true
 end
